a7f6901abcc06a73004f140496beb79dbf5dda0f
William-Thomas-ASR <William.Thomas@asr-group.com>
2022-09-13 16:51:25 -0400

    Added documentation to make_data

--- R/make_data.R
+++ R/make_data.R
@@ -1,6 +1,16 @@
 
 
-get_data <- function(refinery_name = refinery,
+#' Make raw data table
+#'
+#' @param refinery_name The name of the refinery we're making the data frame for
+#' @param endpoint The name of the file containing the data being reading in
+#' @param sheet name of worksheet (Vessel, Downtime, Labor, Trucks, Operations)
+#'
+#' @return A data frame containing the data for the specific location, date, and category
+#' @export
+#'
+#' @examples
+make_data <- function(refinery_name = refinery,
                      endpoint = file_name,
                      sheet = sheet) {
 
